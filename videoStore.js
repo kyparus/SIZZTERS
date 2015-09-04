5fe94a5af0154d2e34b9508b5d1872d8e5998508
kyparus <kyparusg@gmail.com>
2015-09-04 17:41:18 +0300

    sum
    de

---
+++ videoStore.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yurii on 30.08.15.
+ */
+
+(function() {
+    var app = angular.module('videoStore', []);
+
+
+    app.controller('vidosController', function () {
+        this.products = gems;
+    });
+
+
+})();
